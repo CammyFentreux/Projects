69a93005e39c28697783198cb9a7cdfafd16b0f9
Joe Herbert <joecherbert@me.com>
2021-04-19 12:46:49 +0100

    Start mysql setup

--- Timetabler/routes/index.js
+++ Timetabler/routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql2');
+const loginDeatils = require('./loginDetails.json');
+const connection = mysql.createConnection(loginDetails);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
