8c9c876ecc391a73bd85aedbb0d20bc14d3b8f72
Cammy Fentreux <cameron1709@live.co.uk>
2021-04-19 14:14:14 +0100

    Progress towards server communication

--- Timetabler/routes/index.js
+++ Timetabler/routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Timetabler' });
 });
 
+router.get('/user', (req, res, next) => res.render('UserClient'))
+router.get('/admin', (req, res, next) => res.render('AdminClient'))
+
 module.exports = router;
