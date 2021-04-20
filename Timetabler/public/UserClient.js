548568792aa44dcbdcf48a83d4db75caa2852058
Cammy Fentreux <cameron1709@live.co.uk>
2021-04-20 15:39:11 +0100

    Formatting changes

--- Timetabler/public/UserClient.js
+++ Timetabler/public/UserClient.js
@@ -12,10 +12,9 @@ let isDragging = false
 function generateTimeTable() {
 	const table     = document.getElementById("tblTimetable")
 	const days      = ["", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-
 	const timeRange = [9, 10, 11, 12, 1, 2, 3, 4, 5]
-	generateTimeTableHead(table, days)
 
+	generateTimeTableHead(table, days)
 	generateTimeTableBody(table, days, timeRange)
 
 }
@@ -48,11 +47,11 @@ function generateTimeTableHead(table, days) {
  * @see generateTimeTable
  */
 function generateTimeTableBody(table, days, timeRange) {
-
 	let tbody = document.createElement("tbody")
-	for (let increment of timeRange) {
 
+	for (let increment of timeRange) {
 		let row = tbody.insertRow()
+
 		for (let day of days) {
 			if (day === "") {  // create row headers
 				const th = document.createElement("th")
@@ -60,16 +59,14 @@ function generateTimeTableBody(table, days, timeRange) {
 				row.appendChild(th)
 			} else {           // create normal cells
 				const cell = row.insertCell()
+				cell.id    = (day + increment).toLowerCase()
 				cell.classList.add('timetable-region')
-				cell.id = day + increment
 				cell.setAttribute('tabindex', '0')
-				queryAvailability(day.toLowerCase() + increment, cell)
+				queryAvailability(cell)
 			}
 		}
-
 	}
 	table.appendChild(tbody)
-
 }
 /**
  * Saves the current calendar
@@ -93,10 +90,8 @@ function saveAvailabilities() {
 			}
 		} else {
 			console.log("Save Failed on clearUserAvailability")
-			console.error(err)
 		}
 	}, "calendar=" + calendar)
-
 }
 /**
  * Saves a single cell of the calendar
@@ -110,9 +105,8 @@ function saveAvailability(free, datetime) {
 			console.log("Save Successful")
 		} else {
 			console.log("Save Failed on saveUserAvailability")
-			console.error(err)
 		}
-	}, "calendar=" + calendar + "&datetime=" + datetime.toLowerCase() + "&free=" + free)
+	}, "calendar=" + calendar + "&datetime=" + datetime + "&free=" + free)
 
 }
 /**
@@ -139,6 +133,7 @@ function toggleTblCellClass(cell) {
 	if (cell.classList.contains('freetime')) {
 		cell.classList.remove('freetime')
 		cell.classList.add('busy')
+
 	} else if (cell.classList.contains('busy')) {
 		cell.classList.remove('busy')
 	} else {
@@ -173,8 +168,8 @@ window.addEventListener('load', () => {
 // Checks for when the mouse clicked to begin the drag selection of cells
 window.addEventListener('mousedown', function(e) {
 	if (e.target.classList.contains('timetable-region')) {
-		e.target.setAttribute('current-drag', 'true')
 		isDragging = true
+		e.target.setAttribute('current-drag', 'true')
 		e.preventDefault()
 		return toggleTblCellClass(e.target)
 	}
@@ -192,7 +187,6 @@ window.addEventListener('mousemove', function(e) {
 window.addEventListener('mouseup', function(e) {
 	if (isDragging) {
 		isDragging = false
-		document.querySelectorAll('.timetable-region[current-drag]')
-				.forEach(e => e.removeAttribute('current-drag'))
+		document.querySelectorAll('.timetable-region[current-drag]').forEach(e => e.removeAttribute('current-drag'))
 	}
 })
